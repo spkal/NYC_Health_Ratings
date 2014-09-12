366d64144518a346faf66b6c66495b7a46364237
spkal <spk@seapowet.org>
2014-09-11 18:07:29 -0700

    Summerize and add Days. -spk

--- work01.R
+++ work01.R
@@ -6,3 +6,10 @@ library("dplyr")
 nycData_sorted <- arrange(nycData, CAMIS, INSPDATE, CURRENTGRADE)
 nycData_with_grade <- filter(nycData_sorted, CURRENTGRADE != "")
 nycData_by_date <- group_by(nycData_with_grade, CAMIS, GRADEDATE)
+nycData_Grade_Date <- summarize(nycData_by_date,
+    Grade=first(CURRENTGRADE), Date=first(GRADEDATE))
+#   Grade=first(CURRENTGRADE), Date=as.Date(first(as.character(GRADEDATE))))
+
+# Days between grades:
+nycData_Grade_Date <- mutate(nycData_Grade_Date, Days=Date - lag(Date))
+
